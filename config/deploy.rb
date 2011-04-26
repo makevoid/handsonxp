569def2bacc5d7ba443e422a9167698bbf7df179
Francesco Canessa <makevoid@gmail.com>
2011-04-26 19:24:55 +0200

    deh

--- config/deploy.rb
+++ config/deploy.rb
@@ -53,7 +53,7 @@ namespace :deploy do
   
   desc "Create some symlinks from shared to public"
   task :create_symlinks do
-    run "cd #{current_path}/public; ln -s #{deploy_to}/shared/projects_src projects_src"
+    run "cd #{current_path}/public; ln -s #{deploy_to}/shared/creations creations"
   end
   
 end
